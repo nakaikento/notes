885012a603eb6fd5719314dfdff8243b0c0a5fdc
nakaikento <okamoto2okamoto@gmail.com>
2019-12-03 21:11:15 +0900

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Note from './components/Note';
+import Notification from './components/Notification';
+import Footer from './components/Footer';
+import noteService from './services/notes'
+import './index.css'
 
-function App() {
+const App = (props) => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('some error happend...')
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+    }, [])
+    console.log('render', notes.length, 'notes')
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3301/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changeNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changeNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote ))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  const rows = () => notesToShow.map(note =>
+    <Note
+      key={note.id}
+      note={note}
+      toggleImportance={() => toggleImportanceOf(note.id)}
+    />)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      // id: notes.length + 1,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+
+      <Notification message={errorMessage} />
+
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'importtant' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {rows()}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+      <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
